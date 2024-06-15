d06ee58b0f18f581a5d9ab450b401242783c4dd2
sadatcse <sadatcse@gmail.com>
2024-06-15 17:03:55 +0600

    update

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/?retryWrites=true&w=majority&appName=Cluster0`;
+
+console.log(uri);
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+    
+    const Newscollection = client.db('gym').collection('news');
+    const Trainercollection = client.db('gym').collection('trainer');
+  const userCollection = client.db('gym').collection('User');
+
+  //news collection
+
+  app.get('/news/get-id/:id', async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    try {const result = await Newscollection.findOne(filter);
+      res.send(result);} catch (err) {res.status(500).send({ error: err.message });}
+  });
+
+  app.get('/news/get-all', async (req, res) => {
+    try {const cursor = Newscollection.find();
+      const user = await cursor.toArray();
+      res.send(user);} 
+    catch (error) {
+      console.error("Error fetching all contests:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
+        
+
+// Send a ping to confirm a successful connection
+ await client.db("admin").command({ ping: 1 });
+ console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to our server')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+})
